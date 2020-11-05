263bb7f441c89b67fc3d0668b6a9668696b3fe8c
Marco <markoka1@outlook.it>
2020-11-05 16:39:04 +0100

    miglioramenti

--- jSnack2/action.js
+++ jSnack2/action.js
@@ -3,13 +3,15 @@ $(document).ready(function(){
     var nomi_intervallo = []; //Creo un array vuoto, che riempirò con i nomi contenuti nelle posizioni dell'intervallo dato da utente.
     var l = nomi.length; //Salvo la lunghezza dell'Array in una variabile l.
     console.log(nomi); //Stampo tutti i nomi presenti.
+    var a;
+    var b;
 
     do{ //Faccio inserire l'estremo di sinistra.
-        var a = parseInt(prompt('Inserisci un\'intervallo. Digita il primo valore (piu piccolo): '));
+        a = parseInt(prompt('Inserisci un\'intervallo. Digita il primo valore (piu piccolo): '));
     }while (a > l || a < 1);
 
     do{ //Faccio inserire l'estremo di destra.
-        var b = parseInt(prompt('Digita il secondo valore(piu grande): '));
+        b = parseInt(prompt('Digita il secondo valore(piu grande): '));
     } while (b < a || b > l);
 
     //Riempio il vettore vuoto attraverso questo for, con i che parte dall'estremo di sinistra (ricordando che si conta da 0).
@@ -22,13 +24,21 @@ $(document).ready(function(){
     //Con forEach
     const newArray = [];
 
-        nomi.forEach((nome, index) => {
+    nomi.forEach((nome, index) => {
 
-            if ((a-1) <= index && (b-1) >= index) {
-                newArray.push(nome);
-            }
+        if ((a-1) <= index && (b-1) >= index) {
+            newArray.push(nome);
+        }
 
-        });
+    });
 
     console.log('Con l\'utilizzo di forEach: ' + newArray);
+
+    var arrayFilter = nomi.filter((nome, index) => {
+        return (a-1) <= index && (b-1) >= index;
+    });
+
+    //var arrayFilter = nomi.filter((nome, index) => (a-1) <= index && (b-1) >= index);
+
+    console.log('Con l\'utilizzo di filter: ' + arrayFilter);
 })
